05745cd5f1b9ffa8f76bd2c7199ebc8883671019
jamespark <jamespark@bluehole.net>
2016-09-06 17:39:52 +0900

    modify Sample class

--- sample/HealthCheckClassSample.java
+++ sample/HealthCheckClassSample.java
@@ -1,10 +1,15 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import com.studio4365.spring.healthcheck.HealthCheckInterface;
 
-public class HealthCheckClassSample implements HealthCheckInterface {
+import net.bluehole.billing.dao.master.HealthCheckDao;
 
+public class HealthCheckClassSample implements HealthCheckInterface {
 	private static final String VERSION = "0.1";
+	
+	private HealthCheckDao healthCheckDao;
+	private RedisTemplate<String, Boolean> operations;
 	
 	@Override
 	public String version() {
@@ -21,7 +26,21 @@ public class HealthCheckClassSample implements HealthCheckInterface {
 	 */
 	@Override
 	public Long checkDB(ApplicationContext context) {
-		return null;
+		if (healthCheckDao == null) {
+			// Prepared data access object for Healthcheck from Spring Boot Application
+			healthCheckDao = (HealthCheckDao)context.getBean("healthCheckDao");
+		}
+		long tic = System.currentTimeMillis();
+		int result = -1;
+		try {
+			// probably "SELECT 1"
+			result = healthCheckDao.selectOne();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1L;
+		}
+		long toc = System.currentTimeMillis();
+		return result == 1 ? toc - tic : -1L;
 	}
 
 	/**
@@ -34,7 +53,23 @@ public class HealthCheckClassSample implements HealthCheckInterface {
 	 */
 	@Override
 	public Long checkRedis(ApplicationContext context) {
-		return null;
+		if (operations == null) {
+			// Prepared redis template for Healthcheck from Spring Boot Application
+			operations = (RedisTemplate<String, Boolean>)context.getBean("redisTemplate");
+		}
+		long tic = System.currentTimeMillis();
+		Boolean result = false;
+		try {
+			// set value "true" in "healthcheck" key
+			operations.opsForValue().set("healthCheck", true);
+			// get value from "healthcheck" key and assign at result variable
+			result = operations.opsForValue().get("healthCheck");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1L;
+		}
+		long toc = System.currentTimeMillis();
+		return result == true ? toc - tic : -1L;
 	}
 
 }
